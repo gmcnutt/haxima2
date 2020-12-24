5754c3f4aa29482746083f1a0ff1ae1bdf5ddd9f
gmcnutt <gmcnutt@cableone.net>
2020-12-24 10:34:33 -0700

    Separate splash from main menu code

--- src/play_command.c
+++ src/play_command.c
@@ -11,15 +11,54 @@
 #include "font.h"
 
 const char *MAIN_MENU_FONT_PATH = DATADIR "/" PACKAGE "/avatar.ttf";
-const int MAIN_MENU_FONT_PTSIZE = 128;
+const int SPLASH_FONT_PTSIZE = 128;
+const int MAIN_MENU_FONT_PTSIZE = 64;
 
-void play_command_exec(int argc, char **argv)
+void clear_screen()
 {
-        /* Open the font we want */
-        font_t *font = font_open(MAIN_MENU_FONT_PATH, MAIN_MENU_FONT_PTSIZE);
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255,
+                               SDL_ALPHA_OPAQUE);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+}
 
-        /* Render the title centered. */
+void splash()
+{
+        clear_screen();
+        font_t *font = font_open(MAIN_MENU_FONT_PATH, SPLASH_FONT_PTSIZE);
         const char *text = "Haxima2";
+        SDL_Texture *texture = font_render(font, text);
+        int rw, rh;
+        SDL_GetRendererOutputSize(renderer, &rw, &rh);
+        int tw, th;
+        SDL_QueryTexture(texture, NULL, NULL, &tw, &th);
+        SDL_Rect dest = { (rw - tw) / 2, (rh - th) / 2, tw, th };
+        SDL_RenderCopy(renderer, texture, NULL, &dest);
+        SDL_DestroyTexture(texture);
+        mem_deref(font);
+
+        SDL_RenderPresent(renderer);
+
+        /* Loop until user presses a button */
+        SDL_Event event;
+        while (SDL_WaitEvent(&event)) {
+                if (event.type == SDL_QUIT) {
+                        exit(EXIT_SUCCESS);
+                }
+                if (event.type == SDL_KEYDOWN) {
+                        break;
+                }
+        }
+        
+}
+
+void main_menu()
+{
+        clear_screen();
+        
+        /* Render the first option centered below. */
+        font_t *font = font_open(MAIN_MENU_FONT_PATH, MAIN_MENU_FONT_PTSIZE);
+        const char *text = "New Game";
         SDL_Texture *texture = font_render(font, text);
         int rw, rh;
         SDL_GetRendererOutputSize(renderer, &rw, &rh);
@@ -27,14 +66,31 @@ void play_command_exec(int argc, char **argv)
         SDL_QueryTexture(texture, NULL, NULL, &tw, &th);
         SDL_Rect dest = { (rw - tw) / 2, 0, tw, th };
         SDL_RenderCopy(renderer, texture, NULL, &dest);
-        SDL_RenderPresent(renderer);
         SDL_DestroyTexture(texture);
+        mem_deref(font);
+
+        /* Render the quit option centered below. */
+        font = font_open(MAIN_MENU_FONT_PATH, MAIN_MENU_FONT_PTSIZE);
+        text = "Quit";
+        texture = font_render(font, text);
+        dest.y += th;
+        SDL_GetRendererOutputSize(renderer, &rw, &rh);
+        SDL_QueryTexture(texture, NULL, NULL, &tw, &th);
+        dest.x = (rw - tw) / 2;
+        dest.w = tw;
+        dest.h = th;
+        SDL_RenderCopy(renderer, texture, NULL, &dest);
+        SDL_DestroyTexture(texture);
+        mem_deref(font);
+        
+        SDL_RenderPresent(renderer);
+
         
         /* Loop until user exits */
         SDL_Event event;
         while (SDL_WaitEvent(&event)) {
                 if (event.type == SDL_QUIT) {
-                        break;
+                        exit(EXIT_SUCCESS);
                 }
                 if (event.type == SDL_KEYDOWN) {
                         if (event.key.keysym.sym == SDLK_q) {
@@ -42,6 +98,11 @@ void play_command_exec(int argc, char **argv)
                         }
                 }
         }
+        
+}
 
-        mem_deref(font);
+void play_command_exec(int argc, char **argv)
+{
+        splash();
+        main_menu();
 }
